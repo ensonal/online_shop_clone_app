47a13d25b5c1fcf66402c906add0fd11709903af
enes.onal777@gmail.com <151Git+++>
2022-03-16 04:54:14 +0300

    First push

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:online_shop_clone/constants.dart';
+import 'package:online_shop_clone/models/product.dart';
+import 'package:online_shop_clone/screens/home/components/item_card.dart';
+
+import 'categories.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          child: Text(
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                ?.copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: GridView.builder(
+              itemCount: products.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: kDefaultPadding,
+                crossAxisSpacing: kDefaultPadding,
+                childAspectRatio: 0.75,
+              ),
+              itemBuilder: (context, index) => ItemCard(
+                product: products[index],
+                press: () {},
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
